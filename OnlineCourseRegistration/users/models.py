28bdc0743851336061a9fb40f154802769af288d
suhastanmay <tanmayk4@yahoo.co.in>
2018-10-25 21:04:45 +0530

    pull

--- OnlineCourseRegistration/users/models.py
+++ OnlineCourseRegistration/users/models.py
@@ -23,7 +23,6 @@ class Detail(models.Model):
 	def __str__(self):
 		return self.description
 
-
 class AuditCourse(models.Model):
 	roll = models.CharField(max_length=20)
 	name = models.CharField(max_length=20)
